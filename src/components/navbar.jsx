2301accd5bf2e8ba11cdcb0d3b4e2172334b28d9
Abdulhameed Busari <65597134+0xlarmideh@users.noreply.github.com>
2022-11-14 14:55:39 +0000

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div></div>
+      <div className="links">
+        <a href="#">Home</a>
+        <a href="#">Place to stay</a>
+        <a href="#">Home</a>
+        <a href="#">Community</a>
+      </div>
+      <div></div>
+    </nav>
+  )
+}
